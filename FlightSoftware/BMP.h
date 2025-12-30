3a5fb0a71864f5f83cd9cad83dd4b3b312669401
PannonSat <cansat.ph2028@gmail.com>
2025-12-30 20:06:02 +0100

    Fixed headers

--- FlightSoftware/BMP.h
+++ FlightSoftware/BMP.h
@@ -4,7 +4,4 @@ void BMP_init();
 
 void BMP_run();
 
-float* get_BMP_Data();
-int len_BMP_Data();
-
 extern bool BMP_initialized;
--- FlightSoftware/IMU.h
+++ FlightSoftware/IMU.h
@@ -5,7 +5,5 @@ extern void IMU_init(void);
 extern void IMU_run(void);
 
 extern void IMU_main_logic(void);
-extern float* get_Acceleration(void);
-extern float* get_Gyroscope(void);
 
 extern bool IMU_initialized;
